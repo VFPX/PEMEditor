794a8bac64efb50ddce8083d5155af16318d627f
Doug Hennig <dhennig@stonefield.com>
2018-01-31 12:19:24 -0600

    Updated version number and added support files for Thor updater

--- Source/PemEditorVersion.h
+++ Source/PemEditorVersion.h
@@ -1,6 +1,6 @@
-* 7.20.59
+* 7.30.00
 
-#Define cnVersion         7.20.59
-#Define cdVersionDate     November 19, 2014
-#Define	ccPEMEVERSION     [PEM Editor - 7.20.59 - November 19, 2014 - 20141119]
+#Define cnVersion         7.30.00
+#Define cdVersionDate     January 31, 2018
+#Define	ccPEMEVERSION     [PEM Editor - 7.30.00 - January 31, 2018 - 20180131]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
