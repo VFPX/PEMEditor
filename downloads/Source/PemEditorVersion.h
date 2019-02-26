c0e25ae3038e02d60954ac227f7682e30233c963
Doug Hennig <dhennig@stonefield.com>
2019-02-26 17:14:56 -0600

    Updated version number

--- downloads/Source/PemEditorVersion.h
+++ downloads/Source/PemEditorVersion.h
@@ -1,6 +1,6 @@
-* 7.30.00
+* 7.30.02
 
-#Define cnVersion         7.30.01
-#Define cdVersionDate     February 7, 2018
-#Define	ccPEMEVERSION     [PEM Editor - 7.30.01 - February 7, 2018 - 20180207]
+#Define cnVersion         7.30.02
+#Define cdVersionDate     February 26, 2019
+#Define	ccPEMEVERSION     [PEM Editor - 7.30.02 - February 26, 2019 - 20190226]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
