6041460f88bbf015f65b7938a3f3a0a46d4aaa01
Lutz <Lutz@Scheffler-Software.de>
2022-03-27 21:06:08 +0200

    V7.40, thorupdater

--- downloads/Source/pemeditorversion.h
+++ downloads/Source/pemeditorversion.h
@@ -1,8 +1,8 @@
 * 7.40.00
 
 #Define cnVersion         7.40.00
-#Define cdVersionDate     March 20, 2022
-#Define	ccPEMEVERSION     [PEM Editor - 7.40.00 - March 20, 2022 - 20220320]
+#Define cdVersionDate     March 26, 2022
+#Define	ccPEMEVERSION     [PEM Editor - 7.40.00 - March 26, 2022 - 20220326]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
 
 *!*	* 7.30.02
