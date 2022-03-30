6bc1803e6053a4acac31293a7d861ebe364048ac
Lutz <Lutz@Scheffler-Software.de>
2022-03-30 16:40:06 +0200

    v7.40.02
    recompiled to different folder

--- downloads/Source/pemeditorversion.h
+++ downloads/Source/pemeditorversion.h
@@ -1,8 +1,8 @@
 * 7.40.01
 
-#Define cnVersion         7.40.01
-#Define cdVersionDate     March 29, 2022
-#Define	ccPEMEVERSION     [PEM Editor - 7.40.01 - March 29, 2022 - 20220329]
+#Define cnVersion         7.40.02
+#Define cdVersionDate     March 30, 2022
+#Define	ccPEMEVERSION     [PEM Editor - 7.40.02 - March 30, 2022 - 20220330]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
 
 *!*	* 7.30.02
