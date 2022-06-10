59e01b59e7e18e14c7eabac007542b05057e2ec6
Lutz <Lutz@Scheffler-Software.de>
2022-06-10 16:51:58 +0200

    Version  7.41.03
    Problem with not closed TEXT .. ENDTEXT
     fixed #28

--- downloads/Source/pemeditorversion.h
+++ downloads/Source/pemeditorversion.h
@@ -1,6 +1,6 @@
 * 7.41.00
 
-#Define cnVersion         7.41.02
-#Define cdVersionDate     May 29, 2022
-#Define	ccPEMEVERSION     [PEM Editor - 7.41.02 - May 29, 2022 - 20220529]
+#Define cnVersion         7.41.03
+#Define cdVersionDate     June 10, 2022
+#Define	ccPEMEVERSION     [PEM Editor - 7.41.03 - June 10, 2022 - 20220610]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
