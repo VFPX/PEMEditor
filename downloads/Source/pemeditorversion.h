a4d7fd5a32e607a09c80cf67be9354baa28cbb77
Doug Hennig <doug@doughennig.com>
2022-11-02 17:20:24 -0500

    Fixed error when bringing up variables list

--- downloads/Source/pemeditorversion.h
+++ downloads/Source/pemeditorversion.h
@@ -1,6 +1,6 @@
-* 7.41.05
+* 7.41.06
 
-#Define cnVersion         7.41.05
-#Define cdVersionDate     October 30, 2022
-#Define	ccPEMEVERSION     [PEM Editor - 7.41.05 - October 30, 2022 - 20221030]
+#Define cnVersion         7.41.06
+#Define cdVersionDate     November 2, 2022
+#Define	ccPEMEVERSION     [PEM Editor - 7.41.06 - November 2, 2022 - 20221102]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
